f82a9df24d76b2831167926607c5456797977c5d
Shubham Dutta <56682092+shdutta16@users.noreply.github.com>
2023-08-11 12:28:07 +0200

    Update run_fitterFormatting_DNNcat_array_DNN.sh
    Extended up to mass 900

--- BinwiseScripts/run_fitterFormatting_DNNcat_array_DNN.sh
+++ BinwiseScripts/run_fitterFormatting_DNNcat_array_DNN.sh
@@ -3,14 +3,17 @@
 addname="optimized_allBins"
 
 #indir="/eos/cms/store/group/phys_egamma/shdutta/2018Analysis_reloaded/WorkCopy/for_skim/DNN_skim/"
-indir="/eos/cms/store/group/phys_egamma/shdutta/2018Analysis_reloaded/WorkCopy/for_skim/DNN_skim_DNNbins/"
+indir="/eos/user/s/shdutta/Store/2018Analysis_reloaded/nTuples/WorkCopy/DNN_skimmed_HEM/"
 
 
-dnnBins_mA200="0,0.03,0.05,0.1,0.2,0.39,0.75,0.93,0.98,0.99378,0.99835,1"
-dnnBins_mA300="0,0.03,0.04,0.09,0.13,0.27,0.45,0.71,0.91,0.98,0.99183,1"
-dnnBins_mA400="0,0.07,0.19,0.42,0.64,0.82,0.89,0.98,0.99285,0.99751,0.99885,1"
-dnnBins_mA500="0,0.19,0.3,0.57,0.83,0.95,0.98,0.99228,0.99581,0.99845,0.99964,1"
-dnnBins_mA600="0,0.41,0.57,0.8,0.93,0.98,0.99285,0.99733,0.99846,0.99964,0.99996,1"
+dnnBins_mA200="0, 0.112, 0.19,  0.385, 0.729, 0.908,   0.979,   0.99078, 0.99559, 0.99881, 1"
+dnnBins_mA300="0, 0.04,  0.072, 0.106, 0.209, 0.385,   0.638,   0.863,   0.962,   0.99043, 1"
+dnnBins_mA400="0, 0.255, 0.433, 0.644, 0.857, 0.955,   0.988,   0.99384, 0.99751, 0.99909, 1"
+dnnBins_mA500="0, 0.329, 0.521, 0.746, 0.933, 0.98,    0.99291, 0.99654, 0.99851, 0.99964, 1"
+dnnBins_mA600="0, 0.654, 0.838, 0.941, 0.975, 0.99127, 0.99603, 0.99873, 0.99964, 0.99996, 1"
+dnnBins_mA700="0, 0.619, 0.849, 0.95,  0.986, 0.99334, 0.99696, 0.99861, 0.99964, 0.99999, 1"
+dnnBins_mA800="0, 0.478, 0.719, 0.877, 0.954, 0.988,   0.99581, 0.99856, 0.99974, 0.99999, 1"
+dnnBins_mA900="0, 0.497, 0.705, 0.919, 0.968, 0.99127, 0.996,   0.99864, 0.99964, 0.99999, 1"
 
 
 #outdir="DNN_ntuples4fit_pho_newSig_${addname}_dnnBins_"${str_dnnBins}
@@ -57,7 +60,7 @@ fitterFormatting("$dnnBins","$indir","$outdir","data",0,"nTuple_DoubleEG_skimmed
 // For simple DNN skimmed trees with non-uniform bins
 
 
-fitterFormatting("$dnnBins_mA200","$indir","$outdir","sig",0, "nTuple_2017_2HDMa_mA200_ma150_EOY_skimmedTree.root",	"sig_2HDMa_mA200_ma150")
+fitterFormatting("$dnnBins_mA200","$indir","$outdir","sig",0, "nTuple_2HDMa_mA200_ma150_skimmedTree.root",	"sig_2HDMa_mA200_ma150")
 
 fitterFormatting("$dnnBins_mA200","$indir","$outdir","bkg",0, "nTuple_VHToGG_skimmedTree.root",	"VHToGG_mA200")
 fitterFormatting("$dnnBins_mA200","$indir","$outdir","bkg",0, "nTuple_VBFHToGG_skimmedTree.root",	"VBFHToGG_mA200")
@@ -81,7 +84,7 @@ fitterFormatting("$dnnBins_mA200","$indir","$outdir","data",0,"nTuple_DoubleEG_s
 
 
 
-fitterFormatting("$dnnBins_mA300","$indir","$outdir","sig",0, "nTuple_2017_2HDMa_mA300_ma150_EOY_skimmedTree.root",	"sig_2HDMa_mA300_ma150")
+fitterFormatting("$dnnBins_mA300","$indir","$outdir","sig",0, "nTuple_2HDMa_mA300_ma150_skimmedTree.root",	"sig_2HDMa_mA300_ma150")
 
 fitterFormatting("$dnnBins_mA300","$indir","$outdir","bkg",0, "nTuple_VHToGG_skimmedTree.root",	"VHToGG_mA300")
 fitterFormatting("$dnnBins_mA300","$indir","$outdir","bkg",0, "nTuple_VBFHToGG_skimmedTree.root",	"VBFHToGG_mA300")
@@ -105,7 +108,7 @@ fitterFormatting("$dnnBins_mA300","$indir","$outdir","data",0,"nTuple_DoubleEG_s
 
 
 
-fitterFormatting("$dnnBins_mA400","$indir","$outdir","sig",0, "nTuple_2017_2HDMa_mA400_ma150_EOY_skimmedTree.root",	"sig_2HDMa_mA400_ma150")
+fitterFormatting("$dnnBins_mA400","$indir","$outdir","sig",0, "nTuple_2HDMa_mA400_ma150_skimmedTree.root",	"sig_2HDMa_mA400_ma150")
 
 fitterFormatting("$dnnBins_mA400","$indir","$outdir","bkg",0, "nTuple_VHToGG_skimmedTree.root",	"VHToGG_mA400")
 fitterFormatting("$dnnBins_mA400","$indir","$outdir","bkg",0, "nTuple_VBFHToGG_skimmedTree.root",	"VBFHToGG_mA400")
@@ -129,7 +132,7 @@ fitterFormatting("$dnnBins_mA400","$indir","$outdir","data",0,"nTuple_DoubleEG_s
 
 
 
-fitterFormatting("$dnnBins_mA500","$indir","$outdir","sig",0, "nTuple_2017_2HDMa_mA500_ma150_EOY_skimmedTree.root",	"sig_2HDMa_mA500_ma150")
+fitterFormatting("$dnnBins_mA500","$indir","$outdir","sig",0, "nTuple_2HDMa_mA500_ma150_skimmedTree.root",	"sig_2HDMa_mA500_ma150")
 
 fitterFormatting("$dnnBins_mA500","$indir","$outdir","bkg",0, "nTuple_VHToGG_skimmedTree.root",	"VHToGG_mA500")
 fitterFormatting("$dnnBins_mA500","$indir","$outdir","bkg",0, "nTuple_VBFHToGG_skimmedTree.root",	"VBFHToGG_mA500")
@@ -153,7 +156,7 @@ fitterFormatting("$dnnBins_mA500","$indir","$outdir","data",0,"nTuple_DoubleEG_s
 
 
 
-fitterFormatting("$dnnBins_mA600","$indir","$outdir","sig",0, "nTuple_2017_2HDMa_mA600_ma150_EOY_skimmedTree.root",	"sig_2HDMa_mA600_ma150")
+fitterFormatting("$dnnBins_mA600","$indir","$outdir","sig",0, "nTuple_2HDMa_mA600_ma150_skimmedTree.root",	"sig_2HDMa_mA600_ma150")
 
 fitterFormatting("$dnnBins_mA600","$indir","$outdir","bkg",0, "nTuple_VHToGG_skimmedTree.root",	"VHToGG_mA600")
 fitterFormatting("$dnnBins_mA600","$indir","$outdir","bkg",0, "nTuple_VBFHToGG_skimmedTree.root",	"VBFHToGG_mA600")
@@ -173,6 +176,74 @@ fitterFormatting("$dnnBins_mA600","$indir","$outdir","bkg",0, "nTuple_TGJets_ski
 fitterFormatting("$dnnBins_mA600","$indir","$outdir","bkg",0, "nTuple_TTWJetsToLNu_skimmedTree.root", "TTWJetsToLNu_mA600")
 
 fitterFormatting("$dnnBins_mA600","$indir","$outdir","data",0,"nTuple_DoubleEG_skimmedTree.root",	"Data_mA600")
+
+
+
+fitterFormatting("$dnnBins_mA700","$indir","$outdir","sig",0, "nTuple_2HDMa_mA700_ma150_skimmedTree.root",	"sig_2HDMa_mA700_ma150")
+
+fitterFormatting("$dnnBins_mA700","$indir","$outdir","bkg",0, "nTuple_VHToGG_skimmedTree.root",	"VHToGG_mA700")
+fitterFormatting("$dnnBins_mA700","$indir","$outdir","bkg",0, "nTuple_VBFHToGG_skimmedTree.root",	"VBFHToGG_mA700")
+fitterFormatting("$dnnBins_mA700","$indir","$outdir","bkg",0, "nTuple_ttHJetToGG_skimmedTree.root",	"ttHJetToGG_mA700")
+fitterFormatting("$dnnBins_mA700","$indir","$outdir","bkg",0, "nTuple_GluGluHToGG_skimmedTree.root",	"GluGluHToGG_mA700")
+
+fitterFormatting("$dnnBins_mA700","$indir","$outdir","bkg",0, "nTuple_DiPhotonJetsBox_skimmedTree.root",  "DiPhoton_mA700")
+fitterFormatting("$dnnBins_mA700","$indir","$outdir","bkg",0, "nTuple_QCD_skimmedTree.root",		    "QCD_mA700")
+fitterFormatting("$dnnBins_mA700","$indir","$outdir","bkg",0, "nTuple_GJet_skimmedTree.root",             "GJets_mA700")
+fitterFormatting("$dnnBins_mA700","$indir","$outdir","bkg",0, "nTuple_DYJetsToLL_skimmedTree.root",	    "DYJetsToLL_mA700")
+
+fitterFormatting("$dnnBins_mA700","$indir","$outdir","bkg",0, "nTuple_TTGJets_skimmedTree.root",    "TTGJets_mA700")
+fitterFormatting("$dnnBins_mA700","$indir","$outdir","bkg",0, "nTuple_TTGG_0Jets_skimmedTree.root", "TTGG_0Jets_mA700")
+fitterFormatting("$dnnBins_mA700","$indir","$outdir","bkg",0, "nTuple_TTJets_skimmedTree.root",     "TTJets_mA700")
+fitterFormatting("$dnnBins_mA700","$indir","$outdir","bkg",0, "nTuple_TGJets_skimmedTree.root",     "TGJets_mA700")
+
+fitterFormatting("$dnnBins_mA700","$indir","$outdir","bkg",0, "nTuple_TTWJetsToLNu_skimmedTree.root", "TTWJetsToLNu_mA700")
+
+fitterFormatting("$dnnBins_mA700","$indir","$outdir","data",0,"nTuple_DoubleEG_skimmedTree.root",	"Data_mA700")
+
+
+
+fitterFormatting("$dnnBins_mA800","$indir","$outdir","sig",0, "nTuple_2HDMa_mA800_ma150_skimmedTree.root",	"sig_2HDMa_mA800_ma150")
+
+fitterFormatting("$dnnBins_mA800","$indir","$outdir","bkg",0, "nTuple_VHToGG_skimmedTree.root",	"VHToGG_mA800")
+fitterFormatting("$dnnBins_mA800","$indir","$outdir","bkg",0, "nTuple_VBFHToGG_skimmedTree.root",	"VBFHToGG_mA800")
+fitterFormatting("$dnnBins_mA800","$indir","$outdir","bkg",0, "nTuple_ttHJetToGG_skimmedTree.root",	"ttHJetToGG_mA800")
+fitterFormatting("$dnnBins_mA800","$indir","$outdir","bkg",0, "nTuple_GluGluHToGG_skimmedTree.root",	"GluGluHToGG_mA800")
+
+fitterFormatting("$dnnBins_mA800","$indir","$outdir","bkg",0, "nTuple_DiPhotonJetsBox_skimmedTree.root",  "DiPhoton_mA800")
+fitterFormatting("$dnnBins_mA800","$indir","$outdir","bkg",0, "nTuple_QCD_skimmedTree.root",		    "QCD_mA800")
+fitterFormatting("$dnnBins_mA800","$indir","$outdir","bkg",0, "nTuple_GJet_skimmedTree.root",             "GJets_mA800")
+fitterFormatting("$dnnBins_mA800","$indir","$outdir","bkg",0, "nTuple_DYJetsToLL_skimmedTree.root",	    "DYJetsToLL_mA800")
+
+fitterFormatting("$dnnBins_mA800","$indir","$outdir","bkg",0, "nTuple_TTGJets_skimmedTree.root",    "TTGJets_mA800")
+fitterFormatting("$dnnBins_mA800","$indir","$outdir","bkg",0, "nTuple_TTGG_0Jets_skimmedTree.root", "TTGG_0Jets_mA800")
+fitterFormatting("$dnnBins_mA800","$indir","$outdir","bkg",0, "nTuple_TTJets_skimmedTree.root",     "TTJets_mA800")
+fitterFormatting("$dnnBins_mA800","$indir","$outdir","bkg",0, "nTuple_TGJets_skimmedTree.root",     "TGJets_mA800")
+
+fitterFormatting("$dnnBins_mA800","$indir","$outdir","bkg",0, "nTuple_TTWJetsToLNu_skimmedTree.root", "TTWJetsToLNu_mA800")
+
+fitterFormatting("$dnnBins_mA800","$indir","$outdir","data",0,"nTuple_DoubleEG_skimmedTree.root",	"Data_mA800")
+
+
+fitterFormatting("$dnnBins_mA900","$indir","$outdir","sig",0, "nTuple_2HDMa_mA900_ma150_skimmedTree.root",	"sig_2HDMa_mA900_ma150")
+
+fitterFormatting("$dnnBins_mA900","$indir","$outdir","bkg",0, "nTuple_VHToGG_skimmedTree.root",	"VHToGG_mA900")
+fitterFormatting("$dnnBins_mA900","$indir","$outdir","bkg",0, "nTuple_VBFHToGG_skimmedTree.root",	"VBFHToGG_mA900")
+fitterFormatting("$dnnBins_mA900","$indir","$outdir","bkg",0, "nTuple_ttHJetToGG_skimmedTree.root",	"ttHJetToGG_mA900")
+fitterFormatting("$dnnBins_mA900","$indir","$outdir","bkg",0, "nTuple_GluGluHToGG_skimmedTree.root",	"GluGluHToGG_mA900")
+
+fitterFormatting("$dnnBins_mA900","$indir","$outdir","bkg",0, "nTuple_DiPhotonJetsBox_skimmedTree.root",  "DiPhoton_mA900")
+fitterFormatting("$dnnBins_mA900","$indir","$outdir","bkg",0, "nTuple_QCD_skimmedTree.root",		    "QCD_mA900")
+fitterFormatting("$dnnBins_mA900","$indir","$outdir","bkg",0, "nTuple_GJet_skimmedTree.root",             "GJets_mA900")
+fitterFormatting("$dnnBins_mA900","$indir","$outdir","bkg",0, "nTuple_DYJetsToLL_skimmedTree.root",	    "DYJetsToLL_mA900")
+
+fitterFormatting("$dnnBins_mA900","$indir","$outdir","bkg",0, "nTuple_TTGJets_skimmedTree.root",    "TTGJets_mA900")
+fitterFormatting("$dnnBins_mA900","$indir","$outdir","bkg",0, "nTuple_TTGG_0Jets_skimmedTree.root", "TTGG_0Jets_mA900")
+fitterFormatting("$dnnBins_mA900","$indir","$outdir","bkg",0, "nTuple_TTJets_skimmedTree.root",     "TTJets_mA900")
+fitterFormatting("$dnnBins_mA900","$indir","$outdir","bkg",0, "nTuple_TGJets_skimmedTree.root",     "TGJets_mA900")
+
+fitterFormatting("$dnnBins_mA900","$indir","$outdir","bkg",0, "nTuple_TTWJetsToLNu_skimmedTree.root", "TTWJetsToLNu_mA900")
+
+fitterFormatting("$dnnBins_mA900","$indir","$outdir","data",0,"nTuple_DoubleEG_skimmedTree.root",	"Data_mA900")
 
 
 
