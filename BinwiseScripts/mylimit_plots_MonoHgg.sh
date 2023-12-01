d8da8c029c8d0602eba9598f3eaf7336242c550e
Shubham Dutta <56682092+shdutta16@users.noreply.github.com>
2023-12-01 18:29:47 +0530

    Update mylimit_plots_MonoHgg.sh
    Made changes to the file format for saveAs argument.

--- BinwiseScripts/mylimit_plots_MonoHgg.sh
+++ BinwiseScripts/mylimit_plots_MonoHgg.sh
@@ -6,6 +6,6 @@ outDir=$PWD
 
 set -x
 for mDM in 150; do
-    ./mylimit_plots_MonoHgg_2HDMa.py --do-limits -M AsymptoticLimits --input-dir $target --model 2HDMa -O $outDir/ --label MonoHgg_2HDMa --lumi 59.69 --use-fb --mDM $mDM --suffix "" --blind --addName $nm_suffix --saveas pdf,png,root
+    ./mylimit_plots_MonoHgg_2HDMa.py --do-limits -M AsymptoticLimits --input-dir $target --model 2HDMa -O $outDir/ --label MonoHgg_2HDMa --lumi 59.69 --use-fb --mDM $mDM --suffix "" --blind --addName $nm_suffix --saveas root #--saveas pdf,png,root
 done
 set +x
